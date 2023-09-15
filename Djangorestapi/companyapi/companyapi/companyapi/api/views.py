66c5d1c0e39ef114536d2e529d4f81fe2e4ad99d
ANKIT SINDORIYA <105932076+Ankitsindoriya@users.noreply.github.com>
2023-09-15 23:49:59 +0530

    Delete Djangorestapi/companyapi/companyapi/companyapi directory

--- Djangorestapi/companyapi/companyapi/companyapi/api/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from api.models import Company
-from api.serializers import Companyserializers
-
-# Create your views here.
-class Companyviewset(viewsets.ModelViewSet):
-    queryset = Company.objects.all()
-    serializer_class = Companyserializers
